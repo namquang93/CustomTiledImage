ec8970be1b0f72e9608d7115b0692f767d5c48c9
Nam Nguyễn Quang <ngqnam@gmail.com>
2020-08-16 21:37:13 +0700

    First commit with working horizontally flip tiled image.

---
+++ Assets/Scripts/Editor/CustomTiledImageEditor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.UI;
+using UnityEngine;
+
+[CustomEditor(typeof(CustomTiledImage))]
+public class CustomTiledImageEditor : ImageEditor {
+    private SerializedProperty m_FlipHorizontal;
+
+    protected override void OnEnable() {
+        base.OnEnable();
+        
+        m_FlipHorizontal = serializedObject.FindProperty("m_FlipHorizontal");
+    }
+    
+    public override void OnInspectorGUI() {
+        base.OnInspectorGUI();
+        
+        EditorGUILayout.Space();
+        EditorGUILayout.LabelField("AlleyLabs Customized Stuffs", EditorStyles.boldLabel);
+        EditorGUILayout.PropertyField(m_FlipHorizontal, new GUIContent("Flip Horizontal When Tiled"));
+        
+//        serializedObject.Update();
+        serializedObject.ApplyModifiedProperties();
+    }
+}
